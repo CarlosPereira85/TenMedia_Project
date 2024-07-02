c2c86f91c7da414fe2e5e2c3465e565e8a49323f
CarlosPereira85 <caiqueberlin@gmail>
2024-07-02 15:43:56 +0200

    update4

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,7 +6,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class CategoriesController extends Controller
+class CategoryController extends Controller
 {
     public function index()
     {
