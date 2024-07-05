932f609fd89216589a3937a4ad5421c5d50db857
CarlosPereira85 <caiqueberlin@gmail>
2024-07-05 09:24:08 +0200

    update5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,20 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    <!-- Other head content -->
+ 
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+    
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('frontend/css/home.css') }}">
+    
+
 </head>
 <body>
     <div class="container">
         @include('partials.navbar')
         @yield('content')
     </div>
+    <div>
+   
+    </div>
 
     <!-- Scripts -->
+    <script src="{{ asset('frontend/js/jquery-3.7.1.min.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Welcome, {{ $user->name }}</div>
+                <div class="card-header">
+                    Welcome, {{ $user->name }}
+                </div>
 
                 <div class="card-body">
                     <p>This is your personalized home page.</p>
@@ -14,30 +16,27 @@
                     <h1>My Job Applications</h1>
 
                     @if($applications->isEmpty())
-                        <p>You have not applied for any jobs yet.</p>
+                        <p class="text-muted">You have not applied for any jobs yet.</p>
                     @else
-                        <ul>
+                        <ul class=display-flex >
                             @foreach($applications as $application)
                                 <li>
                                     <strong>Job Title:</strong> {{ $application->job->title }} <br>
-                                    <strong>Resume:</strong> <a href="{{ asset('storage/' . $application->resume) }}" target="_blank">View Resume</a> <br>
                                     <strong>Cover Letter:</strong> {{ $application->cover_letter }}
+                                    <strong>Resume:</strong> <a href="{{ asset('storage/' . $application->resume) }}" target="_blank">View Resume</a> <br>
                                 </li>
                             @endforeach
                         </ul>
                     @endif
 
                     <hr>
-
-                    
                 </div>
             </div>
-            
         </div>
     </div>
     <form method="POST" action="{{ route('delete.account') }}" onsubmit="return confirm('Are you sure you want to delete your account? This action cannot be undone.');">
-                        @csrf
-                        <button style="position: absolute; right: 3%; top: 7%;" type="submit" class="btn btn-danger">Delete Account</button>
-                    </form>
+        @csrf
+        <button type="submit" class="btn btn-danger">Delete Account</button>
+    </form>
 </div>
 @endsection
